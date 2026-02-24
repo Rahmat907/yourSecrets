4e2346004a78ce6f99643af4ffa61eb8b259e7e9
Mohammad Rahmat <r0786j0786r0786a0786@gmail.com>
2026-02-24 22:22:17 +0530

    email boilterplate done

--- app/helpers/sendVerifacationmail.ts
+++ app/helpers/sendVerifacationmail.ts
@@ -1,2 +1,2 @@
 import { resend } from "../lib/resend";
-
+import VerificationEmail from "@/email/Email";
---
+++ app/types/Apiresponse.ts
@@ -0,0 +1,6 @@
+import { Message } from "../models/user.model";
+export interface ApiResponse{
+    success : boolean,
+    message : string,
+    isAcceptingMessage?: boolean,
+}
